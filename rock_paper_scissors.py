1c06d1a279863697f01233988b8d01ecf80f54dd
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-03-03 02:53:41 +0530

    rock_paper_scissors_game_final

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,10 +1,10 @@
 import random
 
 user_wins = 0
-compute_wis = 0
+computer_wins = 0
 options = ["rock","paper","scissors"]
 while True:
-    user_input = input("Choss Rock/Paper/Scissors or Q to quit the game. ").lower()
+    user_input = input("Choose Rock/Paper/Scissors or Q to quit the game. ").lower()
     if user_input =='q':
         break
     if user_input not in options:
@@ -14,5 +14,29 @@ while True:
     random_number=random.randint(0,2)
     # rock is 0, paper is 1, scissors is 2
     computer_pick = options[random_number]
-    print('Compuer picked',computer_pick)
+    print("You picked",user_input+".")
+    print('Computer picked', computer_pick + ".")
+    
+    if user_input=='rock' and computer_pick == 'scissors':
+        print("you won!")
+        user_wins+=1
+        
+    
+    elif user_input=='paper' and computer_pick == 'rock':
+        print("you won!")
+        user_wins+=1
+        
+    
+    elif user_input=='scissors' and computer_pick == 'paper':
+        print("you won!")
+        user_wins+=1
+    elif user_input == computer_pick:
+        print("You tie")
+    else:
+        print("you lost!")
+        computer_wins+=1
+
+print(f"You won {user_wins} times")
+print(f"Computer wins {computer_wins} times")
+    
 print("GoodBye")
